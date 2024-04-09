27a3e67df266ec384dcb3812c3cda4400cf34bec
KK <59116986+kk445@users.noreply.github.com>
2024-04-09 16:01:40 +0700

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -5,14 +5,17 @@ terraform {
       version = "~> 4.16"
     }
   }
-
+  
+  # checks terraform cli version
   required_version = ">= 1.2.0"
 }
 
+# Configure the AWS Provider
 provider "aws" {
   region  = "us-west-2"
 }
 
+# Create a ec2 instance
 resource "aws_instance" "app_server" {
   ami           = "ami-830c94e3"
   instance_type = "t2.micro"
